f185fd700a15bf292bf3176ab50b161a7a0f95e0
Your Name <jgbharris@hotmail.co.uk>
2021-02-16 22:51:24 +0000

    list rending from API & correct categories there, filter needs to be done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,36 @@
+import React, { useState, useEffect } from "react"
+
 function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+
+  let [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch('api/posts')
+      response = await response.json()
+      console.log(response)
+      setData(response)
+    }
+    fetchMyAPI()
+  }, [])
+
+  console.log("data", data)
+
+  let posts = data.posts
+
+  return (
+    <ul>
+      {posts ? posts.map((post) => (
+          <li key={post.id}>
+          <p>Title: {post.title}</p>
+          <p>Summary: {post.summary}</p>
+          <p>Author: {post.author.name}</p>
+          <p>Date: {post.publishDate}</p>
+          <p>Categories: {post.categories.map((category, index) => <a key={index}>{category.name}</a>)}</p>
+          </li>
+      )) : null}
+    </ul>
+  )
 }
 
 export default App;
